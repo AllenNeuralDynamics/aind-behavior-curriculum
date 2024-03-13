3c6da17b9fff8b737ff7689bddeeb88c6b98acef
jwong-nd <jonathan.wong@alleninstitute.org>
2024-03-13 09:50:32 -0700

    alphabetical import sort breaks abc import

--- src/aind_behavior_curriculum/__init__.py
+++ src/aind_behavior_curriculum/__init__.py
@@ -3,6 +3,7 @@
 __version__ = "0.0.1"
 
 from .base import *  # noqa: F401, F403
-from .curriculum import *  # noqa: F401, F403
 from .task import *  # noqa: F401, F403
+
+from .curriculum import *  # noqa: F401, F403
 from .trainer import *  # noqa: F401, F403
